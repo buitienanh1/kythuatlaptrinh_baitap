935c0dbb34bc90602c74981c3337cf3f2d352514
tuan anh <ttran8770@gmail.com>
2024-07-24 16:22:38 +0700

    update baitap1 cau 4 và câu 5

--- KTLT_BT/KTLT_BT_Tuan3/BaiTap_Tuan3/BaiTap_Tuan3/BaiTap1.cpp
+++ KTLT_BT/KTLT_BT_Tuan3/BaiTap_Tuan3/BaiTap_Tuan3/BaiTap1.cpp
@@ -2,7 +2,6 @@
 #include<conio.h>
 #include <stdint.h>
 #include <stdio.h>
-
 void TaoMaTran2Chieu(int a[][100], int n, int m) {
 	for (int i = 0; i < n; i++)
 	{
@@ -50,21 +49,32 @@ void xuatPhanTuMAXCot(int a[][100], int n, int m) {
 		Max = 0;
 	}
 }
+int kiemtraDongToanChan(int a[][100], int n, int m)
+{
+	for (int i = 0; i < m; i++)
+	{
+		if (a[n][i] % 2 != 0)
+		{
+			return -1;
+		}
+	}
+	return n;
+}
 void xuatDongChiChuaSoChan(int a[][100], int n, int m) {
 	int x[100];
 	for (int i = 0; i < m; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (a[i][j] % 2 !=0)
+		int ktra = kiemtraDongToanChan(a, i, n);
+		if (ktra != -1) {
+			for (int j = 0; j < n; j++)
 			{
-				x=[];
-				break;
+				printf("%d ", a[i][j]);
 			}
 		}
-		printf("%d ");
-		Max = 0;
+		
+		
 	}
+	
 }
 void sapxep(int a, int b) {
 	int tam = a;
@@ -84,6 +94,40 @@ void sapXepPhanTuTangDan(int a[][100], int n, int m) {
 		
 	}
 }
+void xuatDuongvien(int a[][100], int n, int m) {
+	// Kiểm tra nếu mảng rỗng
+	if (n <= 0 || m <= 0) {
+		printf("Mang rong!\n");
+		return;
+	}
+
+	// In các phần tử trên cùng
+	printf("\ncac phan tu tren cung: ");
+	for (int j = 0; j < m; ++j) {
+		printf("%d ", a[0][j]);
+	}
+	printf("\ncac phan tu ben phai: ");
+	// In các phần tử bên phải
+	for (int i = 1; i < n - 1; ++i) {
+		printf("%d ", a[i][m - 1]);
+	}
+	printf("\ncac phan tu duoi cung: ");
+	// In các phần tử dưới cùng (nếu có)
+	if (n > 1) {
+		for (int j = m - 1; j >= 0; --j) {
+			printf("%d ", a[n - 1][j]);
+		}
+	}
+	printf("\ncac phan tu ben tra : ");
+	// In các phần tử bên trái (nếu có)
+	if (m > 1) {
+		for (int i = n - 2; i > 0; --i) {
+			printf("%d ", a[i][0]);
+		}
+	}
+
+	printf("\n"); // Xuất dòng mới
+}
 void menu() {
 	printf("------------------------Menu----------------------------");
 	printf("\n1.Tao va xuat ma tran a chua cac phan tu ngau nhien.");
@@ -99,30 +143,44 @@ void main() {
 	int m=0;
 	int a[100][100];
 	int luachon;
-	printf("\nNhap Lua chon ban muon su dung:");
-	scanf_s("%d", &luachon);
-	switch (luachon)
+	while (1)
 	{
-	case 1:
-		printf("\nNhap so dong muon tao:");
-		scanf_s("%d", &n);
-		printf("\nnhap so hang muon tao:");
-		scanf_s("%d", &m);
-		TaoMaTran2Chieu(a, n, m);
-		XuatMaTran2chieu(a, n, m);
-	case 2: 
-		printf("\nTong cua tung dong trong mang la: ");
-		xuatTongTungDong(a, n, m);
-	
-	case 3:
-		printf("\nPhan Tu lon nhat cua tung cot trong mang la: ");
-		xuatPhanTuMAXCot(a, n, m);
+		printf("\nNhap Lua chon ban muon su dung:");
+		scanf_s("%d", &luachon);
+		switch (luachon)
+		{
+		case 1:
+			printf("\nNhap so dong muon tao:");
+			scanf_s("%d", &n);
+			printf("\nnhap so hang muon tao:");
+			scanf_s("%d", &m);
+			TaoMaTran2Chieu(a, n, m);
+			XuatMaTran2chieu(a, n, m);
+			break;
+		case 2:
+			printf("\nTong cua tung dong trong mang la: ");
+			xuatTongTungDong(a, n, m);
+			break;
+		case 3:
+			printf("\nPhan Tu lon nhat cua tung cot trong mang la: ");
+			xuatPhanTuMAXCot(a, n, m);
+			break;
+		case 4:
+			xuatDuongvien(a, n, m);
+			break;
 
-	case 6:
-		printf("\n Phan Tu Tang dan theo dong:");
-		sapXepPhanTuTangDan(a, n, m);
-		XuatMaTran2chieu(a, n, m);
-	default:
-		break;
+		case 5:
+			printf("\nDong toan phan tu chan trong mang la: ");
+			xuatDongChiChuaSoChan(a, n, m);
+			break;
+
+		case 6:
+			printf("\n Phan Tu Tang dan theo dong:");
+			sapXepPhanTuTangDan(a, n, m);
+			XuatMaTran2chieu(a, n, m);
+			break;
+		default:
+			break;
+		}
 	}
 }
